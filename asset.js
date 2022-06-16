e2a107128aad4bcc5c0786b302c3bc43e7b601e4
mahtab sharghi <sharghi.mahtab@gmail.com>
2022-06-16 13:17:18 +0430

    asset table added

--- asset.js
+++ asset.js
@@ -47,3 +47,80 @@ chart.draw();
 
 
 
+/* fetch info && render data for asset page */
+
+let exchanges=[];
+
+function exchange_get_data(){
+    return fetch("https://api.coincap.io/v2/exchanges")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(info){
+        exchanges=info.data;
+        return info;
+    });
+}
+
+let ex_list_container=document.querySelector(".white_list_container");
+
+function exchange_render_item(name,volumeUsd,percentTotalVolume,socket){
+    let ex_item_element=document.createElement("div");
+    ex_item_element.classList.add("row");
+    ex_list_container.appendChild(ex_item_element);
+
+
+    var ex_name_element=document.createElement("a");
+    ex_name_element.classList.add("column");
+    ex_name_element.classList.add("asset_column");
+    ex_name_element.setAttribute("href","#");
+    ex_name_element.textContent=name;
+    ex_item_element.appendChild(ex_name_element);
+
+
+    let volumeUsd_element=document.createElement("a");
+    volumeUsd_element.classList.add("column");
+    volumeUsd_element.classList.add("asset_column");
+    volumeUsd_element.setAttribute("href","#");
+    if(volumeUsd!==null){
+        volumeUsd_element.textContent=numbro(volumeUsd).formatCurrency('($ 0.00 a)');
+    }else{
+        volumeUsd_element.textContent="-";
+    }
+    ex_item_element.appendChild(volumeUsd_element);
+
+
+    let percentTotalVolume_element=document.createElement("a");
+    percentTotalVolume_element.classList.add("column");
+    percentTotalVolume_element.classList.add("asset_column");
+    percentTotalVolume_element.setAttribute("href","#");
+    if(percentTotalVolume!==null){
+        percentTotalVolume_element.textContent= numbro(percentTotalVolume).format('3a');
+    }else{
+        percentTotalVolume_element.textContent="-";
+    }
+    ex_item_element.appendChild(percentTotalVolume_element);
+
+
+    let socket_element=document.createElement("a");
+    socket_element.classList.add("column");
+    socket_element.classList.add("asset_column");
+    socket_element.setAttribute("href","#");
+    if (socket!==null){
+        socket_element.textContent= "🟢";
+    }else{
+        socket_element.textContent= "🔴";
+        }
+    ex_item_element.appendChild(socket_element);
+};
+
+
+function exchange_render_list(list){
+    list.forEach(function(item){
+        exchange_render_item(item.name,item.volumeUsd,item.percentTotalVolume,item.socket); 
+    });
+};
+
+exchange_get_data().then(function(){
+    exchange_render_list(exchanges);        
+});
